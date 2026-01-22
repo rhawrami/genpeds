d8f404e9ba436c069a587c66e175e3ab1c89f7a8
r h <ravanhawrami@gmail.com>
2026-01-22 11:11:01 -0600

    add 2024 data support

--- src/genpeds/__init__.py
+++ src/genpeds/__init__.py
@@ -1,6 +1,6 @@
 '''Work with gendered NCES IPEDS data: from admissions to graduation'''
 
-__version__ = '1.2.1'
+__version__ = '1.2.2'
 
 from genpeds.core import Characteristics, Admissions, Enrollment, Completion, Cip, Graduation
 from genpeds.downloader import scrape_ipeds_data
